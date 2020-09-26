347fb232852bd12469aa4368982653fd42fbbc4c
sakuma-rina <aa3158148@gmail.com>
2020-09-26 16:38:45 +0900

    itemsテーブルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_22_065339) do
+ActiveRecord::Schema.define(version: 2020_09_25_060604) do
+
+  create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.integer "user_id"
+    t.string "product_name"
+    t.text "description"
+    t.integer "category_id"
+    t.integer "fee_id"
+    t.integer "address_id"
+    t.integer "days_id"
+    t.integer "price"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
