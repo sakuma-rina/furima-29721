7fc7104dd387a70bce138ed76ac624be82c5b429
sakuma-rina <aa3158148@gmail.com>
2020-10-03 18:10:00 +0900

    削除機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: :new
-  before_action :set_params,only: [:show,:edit,:update]
+  before_action :set_params,only: [:show,:edit,:update,:destroy]
 
   def index
     @items = Item.all.order('created_at DESC')
@@ -19,18 +19,20 @@ class ItemsController < ApplicationController
     end
   end
 
-  def show
-  end
-
-  def edit
-  end
-
  def update
   if @item.update(item_params)
     redirect_to root_path
   else
     render :edit
   end
+ end
+
+  def destroy
+    if @item.destroy
+       redirect_to root_path
+    else
+       render :show
+   end
  end 
 
   private
