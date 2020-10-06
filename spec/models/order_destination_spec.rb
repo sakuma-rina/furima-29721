7e08bbfe0585540b298f99495a5d40dc8a5da8bb
sakuma-rina <aa3158148@gmail.com>
2020-10-07 00:22:13 +0900

    テストコードの修正

--- spec/models/order_destination_spec.rb
+++ spec/models/order_destination_spec.rb
@@ -6,10 +6,19 @@ RSpec.describe OrderDestination, type: :model do
       @order_destination = FactoryBot.build(:order_destination)
     end
 
+    it 'post_codeとstate_idとcityとaddressとphone_numberとbuilding_nameは空でも登録できること' do
+      expect(@order_destination).to be_valid
+    end
+
+    it 'building_nameは空でも登録できること' do
+      @order_destination.building_name = nil
+      expect(@order_destination).to be_valid
+    end
+
     it 'post_codeが空では登録できないこと' do
       @order_destination.post_code = nil
       @order_destination.valid?
-      expect(@order_destination.errors.full_messages).to include("Post code can't be blank")
+      expect(@order_destination.errors.full_messages).to include("Post code is invalid")
     end
 
     it 'state_idが空では登録できないこと' do
@@ -42,16 +51,26 @@ RSpec.describe OrderDestination, type: :model do
       expect(@order_destination.errors.full_messages).to include("Phone number can't be blank")
     end
 
-    it 'state_idが1では登録出来ないこと' do
-      @order_destination.state_id = nil
-      @order_destination.valid?
-      expect(@order_destination.errors.full_messages).to include("State can't be blank")
-    end
-
-    it 'post_codeが-はなければ登録出来ないこと' do
+    it 'post_codeはハイフンがなければ登録出来ないこと' do
       @order_destination.post_code = '1234567'
       @order_destination.valid?
-      expect(@order_destination.errors.full_messages).to include('Post code is invalid')
+      expect(@order_destination.errors.full_messages).to include("Post code is invalid")
     end
+
+    it 'phone_numberはハイフンがあると登録出来ないこと' do
+      @order_destination.phone_number = '080-1234-5678'
+      @order_destination.valid?
+      expect(@order_destination.errors.full_messages).to include("Phone number is invalid")
+    end
+
+    it 'phone_numberは11桁以外は登録出来ないこと' do
+      @order_destination.phone_number = '08012345678912'
+      @order_destination.valid?
+      expect(@order_destination.errors.full_messages).to include("Phone number is invalid")
+    end
+
+   
   end
 end
+
+
