ba69aa61342404469d3a63fd57d3d00eeb573b1b
sakuma-rina <aa3158148@gmail.com>
2020-10-07 01:04:21 +0900

    修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,7 +3,7 @@ class OrdersController < ApplicationController
   before_action :set_params, only: [:index, :create]
 
   def index
-    if current_user.id == @item.user_id || @item.order.id.present?
+    if current_user.id == @item.user_id || @item.order.present?
       redirect_to root_path
     else
       @order_destination = OrderDestination.new
